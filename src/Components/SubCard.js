1e00a1b83403b89793a8adacc2a47fd347344be7
Kwabs-97 <khobewayne@gmail.com>
2023-08-12 00:22:54 +0000

    subCard Responsivenss

--- src/Components/SubCard.js
+++ src/Components/SubCard.js
@@ -5,11 +5,12 @@ import classes from "./SubCard.module.css";
 
 function SubCard() {
   return (
-    <aside>
+    <aside className={classes.aside}>
       <div className={classes.container}>
-        <h1>Watch Everywhere</h1>
+        <h2>Watch Everywhere</h2>
         <p>Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.</p>
       </div>
+   
     </aside>
   );
 }
